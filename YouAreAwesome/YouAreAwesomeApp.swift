b9ff1167961f301666974a4e0c0d78bd77c5fe03
Dharunish <dharyugi@gmail.com>
2025-06-28 13:53:36 -0400

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Dharunish Yugeswardeenoo on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
